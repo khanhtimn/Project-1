90992eb1b7a1ee6d12caa5a34e0d50fdb12f775d
khanhtimn <khanhlcbb@gmail.com>
2024-10-23 01:14:11 +0700

    SUM_PAIR_EQUAL_M

--- src/main/java/store/teamti/week4/SUM_PAIR_EQUAL_M.java
+++ src/main/java/store/teamti/week4/SUM_PAIR_EQUAL_M.java
@@ -6,6 +6,24 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * <p>Description:</p>
+ * Cho dãy a1, a2, ..., an trong đó các phần tử đôi một khác nhau và 1 giá trị nguyên dương M. Hãy đếm số Q các cặp (i,j) sao cho 1 <= i < j <= n và ai + aj = M.
+ *
+ * <p>Input:</p>
+ * Dòng 1: ghi n và M (1 <= n, M <= 1000000)
+ * Dòng 2: ghi a1, a2, ..., an
+ * <p>Output:</p>
+ * Ghi ra giá trị Q
+ * <p>Example:</p>
+ * <pre>
+ * Dữ liệu
+ * 5 6
+ * 5 2 1 4 3
+ * Kết quả
+ * 2
+ * </pre>
+ * */
 public class SUM_PAIR_EQUAL_M {
 
     public static void main(String[] args) throws IOException {
